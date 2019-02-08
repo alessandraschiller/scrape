74eee59dba1341bf2c3074fa847a6f21fe298eb4
Alessandra Schiller <a.schiller@umiami.edu>
2019-02-08 14:15:19 -0500

    so hard

---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+var PORT = 3000;
+
+var db = require("./models");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+mongoose.Promise = Promise;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape-ally";
+mongoose.connect(MONGODB_URI);
+
+
+app.get("/scrape", funciton(req, res) {
+  axios.get("https://www.echos.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $("article h2").each(function(i, element) {
+      var results = {};
+
+    results.title = $(this)
+        .children("a")
+        .text();
+        result.link = $(this)
+       .children("a")
+       .attr("href");
+
+    db.Article.create(result)
+      .then(function(dbArticle) {
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+  });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({_id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle){
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT);
+});
